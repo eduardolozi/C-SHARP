64b8db54af0381e6f9840770fc3b5ae150e70fe0
Eduardo Lozano <101532842+eduardolozi@users.noreply.github.com>
2022-07-13 10:19:00 -0300

    Add files via upload

---
+++ Condicionais/Condicionais/Program.cs
@@ -0,0 +1,78 @@
+﻿//-------------------------------------------------------------------------------------------------------------------------------------
+//~~~~~~~~~~CONDICIONAIS~~~~~~~~~~
+int i = 1;
+    int[] vetor = new int[5];
+    vetor[0] = 1;    
+    vetor[1] = 2;
+    vetor[2] = 3;
+    vetor[3] = 4;   
+    vetor[4] = 5;
+
+    string teste = (i == 1 ? "O numero é 1" : "O numero não é 1");
+    Console.WriteLine(teste);
+    //o resto é literalmente igual a C
+//-------------------------------------------------------------------------------------------------------------------------------------
+//.....................................................................................................................................
+//.....................................................................................................................................
+//-------------------------------------------------------------------------------------------------------------------------------------
+//~~~~~~~~~~REPETIÇÕES~~~~~~~~~~
+    //while, do while, for, foreach
+
+    //pode ser usado um comando chamado continue que pula para a seguinte iteração sem realizar as proximas linhas dentro do loop
+    for (int j = 1; j <= 10; j++) Console.WriteLine(j);
+
+    Console.WriteLine("-------------------------");
+    
+    //o foreach percorre um array
+    foreach(int a in vetor) Console.WriteLine(a);
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+//.....................................................................................................................................
+//.....................................................................................................................................
+//-------------------------------------------------------------------------------------------------------------------------------------
+//~~~~~~~~~~ARRAYS E COLLECTIONS~~~~~~~~~~
+    string[] Nomes = new string[3];
+    Nomes[0] = "BUBALU";
+    if(Array.Exists(Nomes, e => e == "BUBALU"))
+    {
+        Console.WriteLine("-------------------------");
+        Console.WriteLine("Nome existe");
+    }
+
+    List<string> Alunos = new List<string>();
+    Alunos.Add("Eduardo Lozano");
+    Alunos.Add("Juan Alejandro");
+    Alunos.Remove("Eduardo Lozano");
+    Alunos.Add("Icaro");
+    Alunos.Add("Eduardo Lozano");
+    Alunos.Sort();
+    Console.WriteLine("-------------------------");
+
+    //Para procurar por um elemento podemos usar exists
+    bool valor = Alunos.Exists(e => e == "Eduardo Lozano");
+    string resposta = (valor == true ? "O aluno existe" : "O aluno não existe");
+    Console.WriteLine(resposta);
+
+    //Para procurar por uma parte de um elemento podemos usar Find e Contains
+    //Console.WriteLine(Alunos.Find(s => s.Contains("Juan")));
+    string verificaNome = Alunos.Find(s => s.Contains("Luis"));
+    if (verificaNome != null) Console.WriteLine("Existe um aluno com o nome "+ verificaNome);
+    else Console.WriteLine("Não existe aluno com esse nome");
+
+    //Para encontrar o indice de um elemento podemos usar o IndexOf, que possui tres formas de implentação
+    //Proucrando pela lista toda:
+    Console.WriteLine("Index do eduardo: " + Alunos.IndexOf("Eduardo Lozano"));
+    //Procurando a partir de um indice:
+    Console.WriteLine("Index do Icaro: " + Alunos.IndexOf("Icaro", 1));
+    //Procurando em um intervalo:
+    Console.WriteLine("Index do Alejandro: " + Alunos.IndexOf("Juan Alejandro", 0, 1));
+
+    Console.WriteLine("Indice do aluno Icaro: " + Alunos.BinarySearch("Icaro")); //para realizar a pesquisa binaria primeiro precisamos ordenar a lista
+    Console.WriteLine("Tamanho da lista: " + Alunos.Count);
+
+    Console.WriteLine("-------------------------");
+    foreach (string l in Alunos) Console.WriteLine(l);
+//-------------------------------------------------------------------------------------------------------------------------------------
+//.....................................................................................................................................
+//.....................................................................................................................................
+//-------------------------------------------------------------------------------------------------------------------------------------
---
+++ Funcoes/Funcoes/Program.cs
@@ -0,0 +1,124 @@
+﻿//~~~~~~~~~~~~~~FUNÇOES~~~~~~~~~~~~~~
+//local tipo retorno nome (parametros)
+//local e tipo nao sao obrigatorios mas é raro nao declarar
+
+/* LOCAL:
+ *      PUBLIC   (pode virar lib)
+ *      PRIVATE  (apenas quem tem acesso ao codigo ve)
+ *      INTERNAL (interno ao namespace)
+ *      PROTECTED
+ */
+
+/* TIPO:
+ *      STATIC
+ *      NÃO STATIC (NAO ESCREVE NADA)
+ */
+
+/* PASSAGEM POR REFERENCIA (MEXE NO ENDEREÇO DA VARIAVEL):
+ * EX: public static void AumentaDois(ref int num) {
+ *          num += 2;
+ *      }
+ *  Na função main devemos declarar a funçao assim: AumentaDois(ref num)
+ */ 
+
+/* PARAMETRO OUT NA FUNÇÃO (PERMITE RETORNAR MAIS DO QUE UMA VARIAVEL):
+ *  EX: public static int Divisao(int dividendo, int divisor, out int resto) {
+ *          int quociente = dividendo/divisor;
+ *          resto = dividendo - (quociente*divisor);
+ *          return quociente;
+ *      }
+ *  Na funçao main devemos declarar a funão assim: quoc = Divisao(divid, divis, out rest);
+ */
+
+/* PARAMS (PASSAGEM DE INUMEROS VALORES PARA A FUNÇAO):
+ * EX: public static int Soma(params Int[] vetorNumeros) {
+ *          if(vetorNumeros.Length == 0 || vetorNumeros.Length == 1) return 0;
+ *          else {
+ *              for(int i = 0; i < vetorNumeros.Legnth; i++) res += vetorNumeros[i];
+ *          }
+ *          return res;
+ *     }
+ * Na funçao main poderiamos ter: 
+ *      Soma(1, 3, 5, 7, 8, 9);
+ *      int[] vet = {1, 3, 5, 7, 9)
+ *      Soma(vet);
+ *      Soma();
+ */
+
+// PascalCase
+
+namespace Funcoes
+{
+    class Program
+    {
+        /*static int Calcula(int a, int b)
+        {
+            return (a + b);
+        }*/
+
+        private static void LerArquivo(int numero)
+        {
+            string caminho = @"C:\Arquivos\arq" + numero + ".txt";
+            if (File.Exists(caminho))
+            {
+                using (StreamReader arquivo = File.OpenText(caminho))
+                {
+                    string linha;
+                    while ((linha = arquivo.ReadLine()) != null)
+                    {
+                        Console.WriteLine(linha);
+                    }
+                }
+                LerArquivo(++numero);
+            } else
+            {
+                Console.WriteLine("Não existem mais arquivos no diretório.");
+            }
+        }
+
+        private static void InsereNomes(ref List<string>list)
+        {
+            int aux;
+            string nome;
+            while(true)
+            {
+                Console.Write("Digite 1 para inserir e 0 para sair: ");
+                aux = int.Parse(Console.ReadLine());
+                if (aux == 0) break;
+                else
+                {
+                    Console.WriteLine("Insira o nome: ");
+                    nome = Console.ReadLine();
+                    list.Add(nome);
+                }
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            //int a = 2, b = 3; 
+            //Console.WriteLine(Calcula(a, b));
+
+            // O @ é pra nao dar incompatibilidade com as barras
+            LerArquivo(1);
+
+            using(StreamWriter arquivo = File.CreateText(@"C:\Arquivos\arq1.txt"))
+            {
+                string content = "Oi eu sou o eduardo e estou mainpulando o arquivo";
+                arquivo.WriteLine(content);
+            }
+            File.AppendAllText(@"C:\Arquivos\arq1.txt", "sim eu sou foda\n");
+            using (StreamWriter arquivo = File.AppendText(@"C:\Arquivos\arq1.txt"))
+            {
+                string content = "Haha manipulei denovo";
+                arquivo.WriteLine(content);
+            }
+
+            // Passando uma lista por referencia
+            List<string> list = new List<string>();
+            InsereNomes(ref list);
+            foreach (string str in list) Console.WriteLine("Nome: {0}", str);
+        }
+    }
+    
+}
